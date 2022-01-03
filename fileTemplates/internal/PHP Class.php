ed6d75193684e8cb07c9f8f09059256969853768
rva <robin.vaucher@proximis.com>
2022-01-03 14:17:20 +0100

    PS-2021.3.1 <robin@robin-Latitude-7310 Create markdown.xml, PHP Class.php, IntelliLang.xml, project.default.xml, diff.xml, Default copy.xml, RBS Change.xml, PHP Constructor.php, Default.xml, file.template.settings.xml, find.xml, code.style.schemes.xml, git.xml, Zen HTML.xml, JavaScript File.js, PHP Getter Method.php, Default.xml, PHP.xml, JavaScript.xml, duplocatorSettings.xml, PHP Overridden Method Body.php, Eclipse copy.xml, Zen XSL.xml, PHP Setter Method.php, Proximis.xml, editor.xml, Proximis.xml, debugger.xml, PHP File.php, updates.xml, baseRefactoring.xml, AMD JavaScript File.js, ide.general.xml, ea_extended.xml, PHP Class Doc Comment.php, Zen CSS.xml, Twig.xml, xpath.xml, keymap.xml, ui.lnf.xml, php.xml, colors.scheme.xml, html_xml.xml, notifications.xml, PHP Trait.php, databaseDrivers.xml, laf.xml, SQL.xml, PHP Function Doc Comment.php, PHPUnit Test.php, parameter.hints.xml, PHP Fluent Setter Method.php, PHP _in listeners_.xml, PHP File Header.php, intentionSettings.xml, Default _1_.xml, proxy.settings.xml, vcs.xml, PHP Interface.php, usageView.xml

---
+++ fileTemplates/internal/PHP Class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (C) ${YEAR} Proximis
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#if (${NAMESPACE})
+namespace ${NAMESPACE};
+
+/**
+ * @name \\${NAMESPACE}\\${NAME}
+ */
+#end
+class ${NAME}
+{
+
+}
